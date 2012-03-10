47510cce8fbbce3603dfeba02ef14dc5496895cc
John Johansen <john.johansen@canonical.com>
2012-03-10 11:25:30 -0800

    AppArmor: Fix oops in policy unpack auditing
    Post unpacking of policy a verification pass is made on x transition
    indexes.  When this fails a call to audit_iface is made resulting in an
    oops, because audit_iface is expecting a valid buffer position but
    since the failure comes from post unpack verification there is none.
    
    Make the position argument optional so that audit_iface can be called
    from post unpack verification.
    
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    
    Conflicts:
    	security/apparmor/policy_unpack.c

--- security/apparmor/policy_unpack.c
+++ security/apparmor/policy_unpack.c
@@ -84,7 +84,7 @@ static void audit_cb(struct audit_buffer *ab, void *va)
  * @new: profile if it has been allocated (MAYBE NULL)
  * @name: name of the profile being manipulated (MAYBE NULL)
  * @info: any extra info about the failure (MAYBE NULL)
- * @e: buffer position info (NOT NULL)
+ * @e: buffer position info
  * @error: error code
  *
  * Returns: %0 or error
@@ -96,13 +96,12 @@ static int audit_iface(struct aa_profile *new, const char *name,
 	struct common_audit_data sa;
 	struct apparmor_audit_data aad = {0,};
 	COMMON_AUDIT_DATA_INIT(&sa, NONE);
-	sa.aad = &aad;
 	if (e)
-		aad.iface.pos = e->pos - e->start;
-	aad.iface.target = new;
-	aad.name = name;
-	aad.info = info;
-	aad.error = error;
+		sa.aad.iface.pos = e->pos - e->start;
+	sa.aad.iface.target = new;
+	sa.aad.name = name;
+	sa.aad.info = info;
+	sa.aad.error = error;
 
 	return aa_audit(AUDIT_APPARMOR_STATUS, profile, GFP_KERNEL, &sa,
 			audit_cb);
